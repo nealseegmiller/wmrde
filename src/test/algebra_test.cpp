10dacff03e1fa15c9079f06b8ae05885aa29e39f
Neal Seegmiller <neal.seegmiller@swri.org>
2017-01-27 16:45:46 -0600

    added test of multMatMat3 to algebra_test.cpp
    use Eigen to verify correctness.

--- src/test/algebra_test.cpp
+++ src/test/algebra_test.cpp
@@ -1,20 +1,47 @@
 #include <gtest/gtest.h>
-#include "wmrde/algebra/matrix.h"
+#include <Eigen/Dense>
 
-//using namespace wmrde;
+#include <wmrde/algebra/linalg3.h>
+#include <wmrde/algebra/matrix.h>
+
+using namespace wmrde;
 
 // Declare a test
-TEST(TestSuite, addTwoInts)
+TEST(TestSuite, addTwoIntsPass)
 {
-  EXPECT_EQ(wmrde::addTwoInts(2,2),4);
+  EXPECT_EQ(addTwoInts(2,2),4);
 }
 
 // Declare another test
 TEST(TestSuite, addTwoIntsFail)
 {
-  EXPECT_EQ(wmrde::addTwoInts(2,2),5);
+  EXPECT_EQ(addTwoInts(2,2),5);
 }
 
+TEST(TestSuite, multMatMat3)
+{
+  Mat3 A,B,C,C2;
+
+  setMat3(1.25,2,3,4,5,6,7,8,9,A);
+  setMat3(9,8,7,6,5,4,3,2,1,B);
+
+  multMatMat3(A,B,C);
+  printf("C = \n%s", Mat3ToString(C).c_str());
+
+  Eigen::Matrix<Real,3,3> A_;
+  Eigen::Matrix<Real,3,3> B_;
+  Eigen::Matrix<Real,3,3> C_;
+
+  //.data() returns a pointer to the data array of the matrix
+  copyMat3ToArray(A, A_.data());
+  copyMat3ToArray(B, B_.data());
+  C_ = A_*B_;
+  copyArrayToMat3(C_.data(), C2);
+
+  printf("C2 = \n%s", Mat3ToString(C2).c_str());
+
+  EXPECT_TRUE(Mat3Equal(C,C2));
+}
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
