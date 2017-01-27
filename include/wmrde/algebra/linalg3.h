cdbd1d4721d06a0e910542f9c351111b125af942
Neal Seegmiller <neal.seegmiller@swri.org>
2017-01-27 10:14:57 -0600

    refactored linalg3.h, not tested yet
    use inline functions instead of macros because they are typesafe
    and should be just as fast.

--- include/wmrde/algebra/linalg3.h
+++ include/wmrde/algebra/linalg3.h
@@ -1,32 +1,26 @@
-//linalg3.h
-//linear algebra for vectors/matrices in R3
-//Vec3 has an extra element for 16 byte alignment for vectorization
-//vectorized functions can only be used on Vec3 & Mat3 arrays!
-//do not use on sub arrays with no extra element
-//Visual Studio vectorizer messages:
-//http://msdn.microsoft.com/en-us/library/jj658585.aspx
-
+//linear algebra functions for vectors/matrices in R3
 
 #ifndef _WMRDE_LINALG3_H_
 #define _WMRDE_LINALG3_H_
 
 #include <cmath> //for sqrt
+#include <sstream>
+#include <iomanip> //for setprecision
 
-#define LINALG3_SIMD 0
+#include <wmrde/common.h>
 
-//array sizes
-#define SIZEVEC3 4
-#define SIZEMAT3 12
+namespace wmrde
+{
 
-#include <wmrde/algebra/matrix.h>
+#define VEC3_SIZE 4
+#define MAT3_SIZE 12
 
-typedef Real Vec3[SIZEVEC3];
-typedef Real Mat3[SIZEMAT3];
+typedef Real Vec3[VEC3_SIZE];
+typedef Real Mat3[MAT3_SIZE]; //contains three Vec3 arrays
 
-//can convert between macros and inline functions
-//macro pros: guaranteed to inline, faster?
-//inline function pros: type safe, lets compiler decide whether or not to inline, can overload (but don't)
-
+//Vec3 has an extra element for 16 byte alignment for SIMD vectorization.
+//Vectorized functions can only be used on Vec3 & Mat3 arrays!
+//Do not use on sub arrays with no extra element.
 
 //Vec3 indices:
 //0
@@ -41,264 +35,390 @@ typedef Real Mat3[SIZEMAT3];
 //2,6,10
 //3,7,11 unused, for 16 byte alignment only
 
-#define COL0 0
-#define COL1 4
-#define COL2 8
+//TODO, undefine these at the end of the header file
+#define MAT3_COL0 0 //first index in column 0 of Mat3
+#define MAT3_COL1 4 //first index in column 1 of Mat3
+#define MAT3_COL2 8 //first index in column 2 of Mat3
 
-//don't vectorize set or copy functions, code 1300 no computation
-//safe to use on sub arrays
 
 //SET FUNCTIONS
-#define setVec3(val0,val1,val2,Dest) do { \
-	(Dest)[0] = (val0); \
-	(Dest)[1] = (val1); \
-	(Dest)[2] = (val2); \
-} while(0)
+inline void setVec3(
+    const Real val, Vec3 dst)
+{
+  dst[0] = val;
+  dst[1] = val;
+  dst[2] = val;
+}
 
-#define setcVec3(val,Dest) do { \
-	(Dest)[0] = (val); \
-	(Dest)[1] = (val); \
-	(Dest)[2] = (val); \
-} while(0)
+inline void setVec3(
+    const Real val0, const Real val1, const Real val2, Vec3 dst)
+{
+	dst[0] = val0;
+	dst[1] = val1;
+	dst[2] = val2;
+}
 
-#define setMat3(val00,val10,val20,val01,val11,val21,val02,val12,val22,Dest) do { \
-	setVec3(val00,val10,val20,Dest); \
-	setVec3(val01,val11,val21,Dest+COL1); \
-	setVec3(val02,val12,val22,Dest+COL2); \
-} while(0)
+//specify elements in column-major order
+inline void setMat3(
+    const Real val00, const Real val10, const Real val20, //column 0
+    const Real val01, const Real val11, const Real val21, //column 1
+    const Real val02, const Real val12, const Real val22, //column 2
+    Mat3 dst)
+{
+  dst[0] = val00;
+  dst[1] = val10;
+  dst[2] = val20;
+//  dst[3] = 0.0; //unused
+  dst[4] = val01;
+  dst[5] = val11;
+  dst[6] = val21;
+//  dst[7] = 0.0; //unused
+  dst[8] = val02;
+  dst[9] = val12;
+  dst[10] = val22;
+//  dst[11] = 0.0; //unused
+}
 
-#define setcMat3(val,Dest) \
-	setMat3(val,val,val, val,val,val, val,val,val, Dest)
+inline void setMat3(
+    const Real val, Mat3 dst)
+{
+	setMat3(val,val,val, val,val,val, val,val,val, dst);
+//  for (size_t i=0; i < MAT3_SIZE; i++) { dst[i] = val; } //TODO, faster?
+}
 
-#define setDiagMat3(val00,val11,val22,Dest) \
-	setMat3(val00,0,0, 0,val11,0, 0,0,val22, Dest)
+//set to diagonal matrix
+inline void setMat3Diag(
+    const Real val00, const Real val11, const Real val22,
+    Mat3 dst)
+{
+  setMat3(val00,0,0, 0,val11,0, 0,0,val22, dst);
+}
 
-#define setIdentityMat3(Dest) \
-	setMat3(1.0,0,0, 0,1.0,0, 0,0,1.0, Dest)
+//set to identity matrix
+inline void setMat3Identity(Mat3 dst)
+{
+	setMat3(1.0,0,0, 0,1.0,0, 0,0,1.0, dst);
+}
 
 //COPY FUNCTIONS
-#define copyVec3(Source,Dest) do { \
-	(Dest)[0] = (Source)[0]; \
-	(Dest)[1] = (Source)[1]; \
-	(Dest)[2] = (Source)[2]; \
-} while(0)
+inline void copyVec3(const Vec3 src, Vec3 dst)
+{
+	dst[0] = src[0];
+	dst[1] = src[1];
+	dst[2] = src[2];
+}
 
-#define copyMat3(Source,Dest) do { \
-	copyVec3(Source,Dest); \
-	copyVec3(Source+COL1,Dest+COL1); \
-	copyVec3(Source+COL2,Dest+COL2); \
-} while(0)
+//copy 3 elements from one array to another
+inline void copy3(const Real* src, Real* dst)
+{
+  dst[0] = src[0];
+  dst[1] = src[1];
+  dst[2] = src[2];
+}
+
+inline void copyMat3(const Mat3 src, Mat3 dst)
+{
+  dst[0] = src[0];
+  dst[1] = src[1];
+  dst[2] = src[2];
+
+  dst[4] = src[4];
+  dst[5] = src[5];
+  dst[6] = src[6];
+
+  dst[8] = src[8];
+  dst[9] = src[9];
+  dst[10] = src[10];
+
+//  for (size_t i=0; i < MAT3_SIZE; i++) { dst[i] = src[i]; } //TODO, faster?
+}
 
 //TRANSPOSE FUNCTIONS
-//pointer AT can't equal A
-#define copyTMat3(A,AT) \
-setMat3((A)[0],(A)[COL1],(A)[COL2], \
-		(A)[1],(A)[1+COL1],(A)[1+COL2], \
-		(A)[2],(A)[2+COL1],(A)[2+COL2], AT)
+//src and dst can't be the same (can't transpose in place)
+inline void copyTMat3(const Mat3 src, Mat3 dst)
+{
+  dst[0] = src[0];
+  dst[1] = src[4];
+  dst[2] = src[8];
+
+  dst[4] = src[1];
+  dst[5] = src[5];
+  dst[6] = src[9];
+
+  dst[8] = src[2];
+  dst[9] = src[6];
+  dst[10] = src[10];
+}
 
 //copy a Mat3 row to Vec3
-#define copyRowToVec3(ri,M,V) do { \
-	(V)[0] = (M)[ri]; \
-	(V)[1] = (M)[ri+COL1]; \
-	(V)[2] = (M)[ri+COL2]; \
-} while(0)
+inline void copyMat3Row(const Mat3 src, const int row_idx, Vec3 dst)
+{
+	dst[0] = src[row_idx + MAT3_COL0];
+	dst[1] = src[row_idx + MAT3_COL1];
+	dst[2] = src[row_idx + MAT3_COL2];
+}
 
-//copy a Vec3 to a Mat3 row
-#define copyVec3ToRow(V,ri,M) do { \
-	(M)[ri] = (V)[0]; \
-	(M)[ri+COL1] = (V)[1]; \
-	(M)[ri+COL2] = (V)[2]; \
-} while(0)
+//copy to/from Mat3 and an array without alignment elements
+//useful for converting to/from Eigen matrices
+inline void copyMat3ToArray(const Mat3 src, Real* dst)
+{
+  dst[0] = src[0];
+  dst[1] = src[1];
+  dst[2] = src[2];
 
+  dst[3] = src[4];
+  dst[4] = src[5];
+  dst[5] = src[6];
+
+  dst[6] = src[8];
+  dst[7] = src[9];
+  dst[8] = src[10];
+}
+
+inline void copyArrayToMat3(const Real* src, Mat3 dst)
+{
+  dst[0] = src[0];
+  dst[1] = src[1];
+  dst[2] = src[2];
+
+  dst[4] = src[3];
+  dst[5] = src[4];
+  dst[6] = src[5];
+
+  dst[8] = src[6];
+  dst[9] = src[7];
+  dst[10] = src[8];
+}
+
+
+inline bool Vec3Equal(const Mat3 mat_a, const Mat3 mat_b)
+{
+  Real tol = 10.0*std::numeric_limits<Real>::epsilon();
+  bool equal = true;
+  equal &= std::abs(mat_a[0] - mat_b[0]) < tol;
+  equal &= std::abs(mat_a[1] - mat_b[1]) < tol;
+  equal &= std::abs(mat_a[2] - mat_b[2]) < tol;
+
+  return equal;
+}
+
+inline bool Mat3Equal(const Mat3 mat_a, const Mat3 mat_b)
+{
+  Real tol = 10.0*std::numeric_limits<Real>::epsilon();
+  bool equal = true;
+  equal &= std::abs(mat_a[0] - mat_b[0]) < tol;
+  equal &= std::abs(mat_a[1] - mat_b[1]) < tol;
+  equal &= std::abs(mat_a[2] - mat_b[2]) < tol;
+
+  equal &= std::abs(mat_a[4] - mat_b[4]) < tol;
+  equal &= std::abs(mat_a[5] - mat_b[5]) < tol;
+  equal &= std::abs(mat_a[6] - mat_b[6]) < tol;
+
+  equal &= std::abs(mat_a[8] - mat_b[8]) < tol;
+  equal &= std::abs(mat_a[9] - mat_b[9]) < tol;
+  equal &= std::abs(mat_a[10] - mat_b[10]) < tol;
+
+  return equal;
+}
 
 //ARITHMETIC FUNCTIONS
-//pointer C can't equal A or B
+#define LINALG3_SIMD 0
 #if LINALG3_SIMD
 //TODO, use for loops for auto-vectorization
 
 #else
-//Vec3
-//ADD
-#define addVec3(A,B,C) do { \
-	(C)[0] = (A)[0] + (B)[0]; \
-	(C)[1] = (A)[1] + (B)[1]; \
-	(C)[2] = (A)[2] + (B)[2]; \
-} while(0)
 
-//add with a multiplier, m = -1 to subtract
-#define addmVec3(A,m,B,C) do { \
-	(C)[0] = (A)[0] + (m)*(B)[0]; \
-	(C)[1] = (A)[1] + (m)*(B)[1]; \
-	(C)[2] = (A)[2] + (m)*(B)[2]; \
-} while(0)
+//VEC3 FUNCTIONS
 
-//add a constant
-#define addcVec3(A,b,C) do { \
-	(C)[0] = (A)[0] + (b); \
-	(C)[1] = (A)[1] + (b); \
-	(C)[2] = (A)[2] + (b); \
-} while(0)
+//add vec_b to vec_a
+inline void addVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
+{
+	out[0] = vec_a[0] + vec_b[0];
+	out[1] = vec_a[1] + vec_b[1];
+	out[2] = vec_a[2] + vec_b[2];
+}
 
+//add m*vec_b to vec_a
+inline void addmVec3(const Vec3 vec_a, const Real m, const Vec3 vec_b, Vec3 out)
+{
+  out[0] = vec_a[0] + m*vec_b[0];
+  out[1] = vec_a[1] + m*vec_b[1];
+  out[2] = vec_a[2] + m*vec_b[2];
+}
 
-//MULTIPLY
-//inline void mulcVec3(const Vec3 A, const Real b, Vec3 C) {
-#define mulcVec3(A,b,C) do { \
-	(C)[0] = (A)[0]*(b); \
-	(C)[1] = (A)[1]*(b); \
-	(C)[2] = (A)[2]*(b); \
-} while(0)
+//subtract vec_b from vec_a
+//TODO, could instead use addmVec3(vec_a, -1.0, vec_b, out)
+inline void subVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
+{
+  out[0] = vec_a[0] - vec_b[0];
+  out[1] = vec_a[1] - vec_b[1];
+  out[2] = vec_a[2] - vec_b[2];
+}
 
-//Mat3
-//ADD
-//inline void addMat3(const Mat3 A, const Mat3 B, Mat3 C) {
-#define addMat3(A,B,C) do { \
-	addVec3(A,B,C); \
-	addVec3(A+COL1,B+COL1,C+COL1); \
-	addVec3(A+COL2,B+COL2,C+COL2); \
-} while(0)
+//multiply a vector by a coefficient
+inline void mulcVec3(const Vec3 vec, const Real coeff, Vec3 out) {
+	out[0] = coeff*vec[0];
+	out[1] = coeff*vec[1];
+	out[2] = coeff*vec[2];
+}
 
-#define addmMat3(A,m,B,C) do { \
-	addmVec3(A,m,B,C); \
-	addmVec3(A+COL1,m,B+COL1,C+COL1); \
-	addmVec3(A+COL2,m,B+COL2,C+COL2); \
-} while(0)
+//MAT3 FUNCTIONS
 
-#define addcMat3(A,b,C) do { \
-	addcVec3(A,b,C); \
-	addcVec3(A+COL1,b,C+COL1); \
-	addcVec3(A+COL2,b,C+COL2); \
-} while(0)
+//add mat_b to mat_a
+inline void addMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
+{
+	out[0] = mat_a[0] + mat_b[0];
+	out[1] = mat_a[1] + mat_b[1];
+	out[2] = mat_a[2] + mat_b[2];
 
+  out[4] = mat_a[4] + mat_b[4];
+  out[5] = mat_a[5] + mat_b[5];
+  out[6] = mat_a[6] + mat_b[6];
 
-//MULTIPLY
-#define mulcMat3(A,b,C) do { \
-	mulcVec3(A,b,C); \
-	mulcVec3(A+COL1,b,C+COL1); \
-	mulcVec3(A+COL2,b,C+COL2); \
-} while(0)
+  out[8] = mat_a[8] + mat_b[8];
+  out[9] = mat_a[9] + mat_b[9];
+  out[10] = mat_a[10] + mat_b[10];
+}
+
+//add m*mat_b to mat_a
+//to subtract use addmMat3(mat_a, -1.0, mat_b, out)
+inline void addmMat3(const Mat3 mat_a, const Real m, const Mat3 mat_b, Mat3 out)
+{
+  out[0] = mat_a[0] + m*mat_b[0];
+  out[1] = mat_a[1] + m*mat_b[1];
+  out[2] = mat_a[2] + m*mat_b[2];
+
+  out[4] = mat_a[4] + m*mat_b[4];
+  out[5] = mat_a[5] + m*mat_b[5];
+  out[6] = mat_a[6] + m*mat_b[6];
+
+  out[8] = mat_a[8] + m*mat_b[8];
+  out[9] = mat_a[9] + m*mat_b[9];
+  out[10] = mat_a[10] + m*mat_b[10];
+}
+
+//multiply a matrix by a coefficient
+inline void mulcMat3(const Mat3 mat, const Real coeff, Mat3 out) {
+  out[0] = coeff*mat[0];
+  out[1] = coeff*mat[1];
+  out[2] = coeff*mat[2];
+
+  out[4] = coeff*mat[4];
+  out[5] = coeff*mat[5];
+  out[6] = coeff*mat[6];
+
+  out[8] = coeff*mat[8];
+  out[9] = coeff*mat[9];
+  out[10] = coeff*mat[10];
+}
 
 #endif
 
 
 //VECTOR OPERATIONS
-#define normVec3(A) sqrt((A)[0]*(A)[0] + (A)[1]*(A)[1] + (A)[2]*(A)[2])
-//must use inline function to precompute normal
-inline void normalizeVec3(Vec3 A) {
-	Real n = normVec3(A);
-	mulcVec3(A,1.0/n,A);
+//return the norm of a vector
+inline Real normVec3(const Vec3 vec)
+{
+  return sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
 }
-#define dotVec3(A,B) ((A)[0]*(B)[0] + (A)[1]*(B)[1] + (A)[2]*(B)[2])
+//normalize a vector
+inline void normalizeVec3(Vec3 vec)
+{
+	mulcVec3(vec, 1.0/normVec3(vec), vec);
+}
+//compute the dot product of 2 vectors
+inline Real dotVec3(const Vec3 vec_a, const Vec3 vec_b)
+{
+  return vec_a[0]*vec_b[0] + vec_a[1]*vec_b[1] + vec_a[2]*vec_b[2];
+}
 
+//compute the cross product of vec_a and vec_b
+inline void crossVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
+{
+	setVec3(
+	    -vec_a[2]*vec_b[1] + vec_a[1]*vec_b[2],
+	     vec_a[2]*vec_b[0] - vec_a[0]*vec_b[2],
+	    -vec_a[1]*vec_b[0] + vec_a[0]*vec_b[1], out);
+}
 
-//C = A x B
-//pointer C can't equal A or B
-#define crossVec3(A,B,C) do { \
-	(C)[0] = -(A)[2]*(B)[1] + (A)[1]*(B)[2]; \
-	(C)[1] =  (A)[2]*(B)[0] - (A)[0]*(B)[2]; \
-	(C)[2] = -(A)[1]*(B)[0] + (A)[0]*(B)[1]; \
-} while(0)
-
-//A x B = skew(A)*B
+//compute skew-symmetric matrix from vector for computing cross products
+//vec_a x vec_b = skew(vec_a)*vec_b
+//skew(vec_a) = [
 //  0, -v2,  v1
 // v2,   0, -v0
-//-v1,  v0,   0 
-#define skewVec3(V,M) do { \
-	(M)[0]= 0;		(M)[0+COL1]=-(V)[2];	(M)[0+COL2]= (V)[1]; \
-	(M)[1]= (V)[2];	(M)[1+COL1]= 0;			(M)[1+COL2]=-(V)[0]; \
-	(M)[2]=-(V)[1];	(M)[2+COL1]= (V)[0];	(M)[2+COL2]= 0; \
-} while(0)
+//-v1,  v0,   0]
+inline void skewVec3(const Vec3 vec, Mat3 out)
+{
+  setMat3(0, vec[2], -vec[1],
+      -vec[2], 0, vec[0],
+       vec[1], -vec[0], 0, out);
+}
 
-#define unskewVec3(M,V) do { \
-	(V)[0] = (M)[6]; \
-	(V)[1] =-(M)[2]; \
-	(V)[2] = (M)[1]; \
-} while(0)
-
-
+inline void unskewVec3(const Mat3 mat, Vec3 out)
+{
+	out[0] = mat[6];
+	out[1] =-mat[2];
+	out[2] = mat[1];
+}
 
 //MATRIX MULTIPLICATION
-//c = A*b
-//pointer c can't equal b
-#if LINALG3_SIMD
-//DEBUGGING, to vectorize
+//TODO, possible to vectorize?
 
-inline void multMatVec3(const Mat3 A, const Vec3 b, Vec3 c) {
-	for (int i=0; i<NVEC3LOOP; i++)
-		c[i] = A[i]*b[0] + (A+COL1)[i]*b[1] + (A+COL2)[i]*b[2]; //not vectorized code 1200
+//out = mat*vec
+//out can't be the same as vec (can't multiply in place)
+//TODO, use setVec3 to allow multiply in place?
+inline void multMatVec3(const Mat3 mat, const Vec3 vec, Vec3 out)
+{
+  out[0] = mat[0]*vec[0] + mat[4]*vec[1] + mat[8]*vec[2];
+  out[1] = mat[1]*vec[0] + mat[5]*vec[1] + mat[9]*vec[2];
+	out[2] = mat[2]*vec[0] + mat[6]*vec[1] + mat[10]*vec[2];
 }
 
-#else
-
-//inline void multMatVec3(const Mat3 A, const Vec3 b, Vec3 c) {
-#define multMatVec3(A,b,c) do { \
-	(c)[0] = (A)[0]*(b)[0] + (A)[0+COL1]*(b)[1] + (A)[0+COL2]*(b)[2]; \
-	(c)[1] = (A)[1]*(b)[0] + (A)[1+COL1]*(b)[1] + (A)[1+COL2]*(b)[2]; \
-	(c)[2] = (A)[2]*(b)[0] + (A)[2+COL1]*(b)[1] + (A)[2+COL2]*(b)[2]; \
-} while(0)
-
-#endif
-
-
-
-//c = (A^T)*b
-//possible to vectorize?
-//inline void multMatTVec3(const Mat3 A, const Vec3 b, Vec3 c) { 
-#define multMatTVec3(A,b,c) do { \
-	(c)[0] = dotVec3(A,b); \
-	(c)[1] = dotVec3(A+COL1,b); \
-	(c)[2] = dotVec3(A+COL2,b); \
-} while(0)
-
-
-//C = A*B
-//inline void multMatMat3(const Mat3 A, const Mat3 B, Mat3 C) { 
-#define multMatMat3(A,B,C) do { \
-	multMatVec3(A,B,C); \
-	multMatVec3(A,B+COL1,C+COL1); \
-	multMatVec3(A,B+COL2,C+COL2); \
-} while(0)
-
-//C = (A^T)*B
-//inline void multMatTMat3(const Mat3 A, const Mat3 B, Mat3 C) {
-#define multMatTMat3(A,B,C) do { \
-	multMatTVec3(A,B,C); \
-	multMatTVec3(A,B+COL1,C+COL1); \
-	multMatTVec3(A,B+COL2,C+COL2); \
-} while(0)
-
-
-
-//copy to/from array without extra elements for alignment
-inline void copyVec3ToArray(const int n, const Vec3 Source[], Real* Dest) {
-	for (int i=0; i<n; i++)
-		copyVec3(Source[i],Dest+(i*3));
+//out = (mat^T)*vec
+inline void multMatTVec3(const Mat3 mat, const Vec3 vec, Vec3 out)
+{
+	out[0] = mat[0]*vec[0] + mat[1]*vec[1] + mat[2]*vec[2];
+	out[1] = mat[4]*vec[0] + mat[5]*vec[1] + mat[6]*vec[2];
+	out[2] = mat[8]*vec[0] + mat[9]*vec[1] + mat[10]*vec[2];
 }
 
-inline void copyArrayToVec3(const Real* Source, const int n, Vec3 Dest[]) {
-	for (int i=0; i<n; i++)
-		copyVec3(Source+(i*3),Dest[i]);
+//out = mat_a * mat_b
+inline void multMatMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
+{
+	multMatVec3(mat_a, &mat_b[MAT3_COL0], &out[MAT3_COL0]);
+	multMatVec3(mat_a, &mat_b[MAT3_COL1], &out[MAT3_COL1]);
+	multMatVec3(mat_a, &mat_b[MAT3_COL2], &out[MAT3_COL2]);
+}
+
+//out = (mat_a^T) * mat_b
+inline void multMatTMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
+{
+  multMatTVec3(mat_a, &mat_b[MAT3_COL0], &out[MAT3_COL0]);
+  multMatTVec3(mat_a, &mat_b[MAT3_COL1], &out[MAT3_COL1]);
+  multMatTVec3(mat_a, &mat_b[MAT3_COL2], &out[MAT3_COL2]);
 }
 
 //PRINT FUNCTIONS
-inline void printVec3(const Vec3 V, int precision, int width) { 
-	printMatReal(3,1,V,precision,width); 
+//TODO, use wmrde::Matrix class for smarter formatting
+inline std::string Vec3ToString(const Vec3 vec, const int precision = 6)
+{
+  std::stringstream ss;
+  ss << std::setprecision(precision);
+  ss << vec[0] << vec[1] << vec[2] << "\n";
+  return ss.str();
 }
 
-inline void printnVec3(const int n, const Vec3 V[], int precision, int width) {
-	//dynamic memory allocation required
-	Real* V_;
-	V_ = new Real[n*3];
+inline std::string Mat3ToString(const Mat3 mat, const int precision = 6)
+{
+  std::stringstream ss;
+  ss << std::setprecision(precision);
+  ss << mat[0] << " " << mat[4] << " " << mat[8] << "\n"
+     << mat[1] << " " << mat[5] << " " << mat[9] << "\n"
+     << mat[2] << " " << mat[6] << " " << mat[10] << "\n";
 
-	copyVec3ToArray(n,V,V_);
-	printMatReal(3,n,V_,precision,width);
-
-	delete[] V_;
+  return ss.str();
 }
 
-inline void printMat3(const Mat3 M, int precision, int width) { 
-	Real M_[3*3];
-	copyVec3ToArray(3,(const Vec3*) M,M_);
-	printMatReal(3,3,M_,precision,width);
-}
+} //namespace
 
 #endif //_WMRDE_LINALG3_H_
