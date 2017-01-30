8fb382533ab3c7884ce13371b62baaa1065a8559
Neal Seegmiller <neal.seegmiller@swri.org>
2017-01-30 12:52:27 -0600

    refactor linalg3.h for SIMD vectorization.
    benchmark computation time in algebra_test.cpp. Computation time
    is slower with vectorization, and linalg3.h is slower than using
    Eigen::Matrix<3,3,Real>

--- include/wmrde/algebra/linalg3.h
+++ include/wmrde/algebra/linalg3.h
@@ -12,11 +12,12 @@
 namespace wmrde
 {
 
+#define LINALG3_SIMD 1
+
+#if LINALG3_SIMD
+
 #define VEC3_SIZE 4
 #define MAT3_SIZE 12
-
-typedef Real Vec3[VEC3_SIZE];
-typedef Real Mat3[MAT3_SIZE]; //contains three Vec3 arrays
 
 //Vec3 has an extra element for 16 byte alignment for SIMD vectorization.
 //Vectorized functions can only be used on Vec3 & Mat3 arrays!
@@ -52,6 +53,34 @@ enum Mat3ColumnIndices {
   MAT3_COL1=4,
   MAT3_COL2=8
 };
+
+#else
+
+#define VEC3_SIZE 3
+#define MAT3_SIZE 9
+
+enum Mat3Indices {
+  MAT3_0=0,
+  MAT3_1=1,
+  MAT3_2=2,
+  MAT3_3=3,
+  MAT3_4=4,
+  MAT3_5=5,
+  MAT3_6=6,
+  MAT3_7=7,
+  MAT3_8=8
+};
+
+enum Mat3ColumnIndices {
+  MAT3_COL0=0,
+  MAT3_COL1=3,
+  MAT3_COL2=6
+};
+
+#endif
+
+typedef Real Vec3[VEC3_SIZE];
+typedef Real Mat3[MAT3_SIZE]; //contains three Vec3 arrays
 
 //SET FUNCTIONS
 inline void setVec3(
@@ -94,11 +123,11 @@ inline void setMat3(
     const Real val, Mat3 dst)
 {
   //set a continuous block of memory
-  for (size_t i=0; i < MAT3_SIZE; i++) { dst[i] = val; }
+  for (size_t i=0; i<MAT3_SIZE; i++) { dst[i] = val; }
 }
 
 //set to diagonal matrix
-inline void setMat3Diag(
+inline void setMat3Diagonal(
     const Real val00, const Real val11, const Real val22,
     Mat3 dst)
 {
@@ -130,7 +159,7 @@ inline void copy3(const Real* src, Real* dst)
 inline void copyMat3(const Mat3 src, Mat3 dst)
 {
   //copy a contiguous block of memory
-  for (size_t i=0; i < MAT3_SIZE; i++) { dst[i] = src[i]; }
+  for (size_t i=0; i<MAT3_SIZE; i++) { dst[i] = src[i]; }
 }
 
 //TRANSPOSE FUNCTIONS
@@ -213,21 +242,41 @@ inline bool Mat3Equal(const Mat3 mat_a, const Mat3 mat_b)
   return equal;
 }
 
-//ARITHMETIC FUNCTIONS
-#define LINALG3_SIMD 0
+//VEC3 ARITHMETIC FUNCTIONS
 #if LINALG3_SIMD
-//TODO, use for loops for auto-vectorization
-
-#else
-
-//VEC3 FUNCTIONS
 
 //add vec_b to vec_a
 inline void addVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
 {
-	out[0] = vec_a[0] + vec_b[0];
-	out[1] = vec_a[1] + vec_b[1];
-	out[2] = vec_a[2] + vec_b[2];
+  //This loop is unrolled but not vectorized
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] = vec_a[i] + vec_b[i]; }
+}
+
+//add m*vec_b to vec_a
+inline void addmVec3(const Vec3 vec_a, const Real m, const Vec3 vec_b, Vec3 out)
+{
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] = vec_a[i] + m*vec_b[i]; }
+}
+
+//subtract vec_b from vec_a
+inline void subVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
+{
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] = vec_a[i] - vec_b[i]; }
+}
+
+//multiply vector by a scalar
+inline void mulcVec3(const Vec3 vec, const Real c, Vec3 out) {
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] = c*vec[i]; }
+}
+
+#else
+
+//add vec_b to vec_a
+inline void addVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
+{
+  out[0] = vec_a[0] + vec_b[0];
+  out[1] = vec_a[1] + vec_b[1];
+  out[2] = vec_a[2] + vec_b[2];
 }
 
 //add m*vec_b to vec_a
@@ -247,14 +296,40 @@ inline void subVec3(const Vec3 vec_a, const Vec3 vec_b, Vec3 out)
   out[2] = vec_a[2] - vec_b[2];
 }
 
-//multiply a vector by a coefficient
-inline void mulcVec3(const Vec3 vec, const Real coeff, Vec3 out) {
-	out[0] = coeff*vec[0];
-	out[1] = coeff*vec[1];
-	out[2] = coeff*vec[2];
+//multiply vector by a scalar
+inline void mulcVec3(const Vec3 vec, const Real c, Vec3 out) {
+  out[0] = c*vec[0];
+  out[1] = c*vec[1];
+  out[2] = c*vec[2];
 }
 
-//MAT3 FUNCTIONS
+#endif
+
+
+//MAT3 ARITHMETIC FUNCTIONS
+
+#if LINALG3_SIMD
+//add mat_b to mat_a
+inline void addMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
+{
+  //This loop is vectorized
+  for (size_t i=0; i<MAT3_SIZE; i++) { out[i] = mat_a[i] + mat_b[i]; }
+}
+
+//add m*mat_b to mat_a
+//to subtract use addmMat3(mat_a, -1.0, mat_b, out)
+inline void addmMat3(const Mat3 mat_a, const Real m, const Mat3 mat_b, Mat3 out)
+{
+  for (size_t i=0; i<MAT3_SIZE; i++) { out[i] = mat_a[i] + m*mat_b[i]; }
+}
+
+//multiply matrix by a scalar constant
+inline void mulcMat3(const Mat3 mat, const Real c, Mat3 out)
+{
+  for (size_t i=0; i<MAT3_SIZE; i++) { out[i] = c*mat[i]; }
+}
+
+#else
 
 //add mat_b to mat_a
 inline void addMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
@@ -289,7 +364,7 @@ inline void addmMat3(const Mat3 mat_a, const Real m, const Mat3 mat_b, Mat3 out)
   out[MAT3_8] = mat_a[MAT3_8] + m*mat_b[MAT3_8];
 }
 
-//multiply a matrix by a scalar constant
+//multiply matrix by a scalar constant
 inline void mulcMat3(const Mat3 mat, const Real c, Mat3 out) {
   out[MAT3_0] = c*mat[MAT3_0];
   out[MAT3_1] = c*mat[MAT3_1];
@@ -354,7 +429,27 @@ inline void unskewVec3(const Mat3 mat, Vec3 out)
 }
 
 //MATRIX MULTIPLICATION
-//TODO, possible to vectorize?
+#if LINALG3_SIMD
+
+//These loops are vectorized but this method is slower!
+inline void multMatVec3(const Mat3 mat, const Vec3 vec, Vec3 out)
+{
+  setVec3(0.0, out);
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] += mat[MAT3_COL0+i]*vec[0]; }
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] += mat[MAT3_COL1+i]*vec[1]; }
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[i] += mat[MAT3_COL2+i]*vec[2]; }
+}
+
+//These loops are unrolled but not vectorized
+inline void multMatTVec3(const Mat3 mat, const Vec3 vec, Vec3 out)
+{
+  setVec3(0.0, out);
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[0] += mat[MAT3_COL0+i]*vec[i]; }
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[1] += mat[MAT3_COL1+i]*vec[i]; }
+  for (size_t i=0; i<VEC3_SIZE; i++) { out[2] += mat[MAT3_COL2+i]*vec[i]; }
+}
+
+#else
 
 //out = mat*vec
 //out can't be the same as vec (can't multiply in place)
@@ -373,6 +468,8 @@ inline void multMatTVec3(const Mat3 mat, const Vec3 vec, Vec3 out)
 	out[1] = mat[MAT3_3]*vec[0] + mat[MAT3_4]*vec[1] + mat[MAT3_5]*vec[2];
 	out[2] = mat[MAT3_6]*vec[0] + mat[MAT3_7]*vec[1] + mat[MAT3_8]*vec[2];
 }
+
+#endif
 
 //out = mat_a * mat_b
 inline void multMatMat3(const Mat3 mat_a, const Mat3 mat_b, Mat3 out)
@@ -396,7 +493,7 @@ inline std::string Vec3ToString(const Vec3 vec, const int precision = 6)
 {
   std::stringstream ss;
   ss << std::setprecision(precision);
-  ss << vec[0] << vec[1] << vec[2] << "\n";
+  ss << vec[0] << "\n" << vec[1] << "\n" << vec[2] << "\n";
   return ss.str();
 }
 
@@ -404,9 +501,9 @@ inline std::string Mat3ToString(const Mat3 mat, const int precision = 6)
 {
   std::stringstream ss;
   ss << std::setprecision(precision);
-  ss << mat[0] << " " << mat[4] << " " << mat[8] << "\n"
-     << mat[1] << " " << mat[5] << " " << mat[9] << "\n"
-     << mat[2] << " " << mat[6] << " " << mat[10] << "\n";
+  ss << mat[MAT3_0] << " " << mat[MAT3_3] << " " << mat[MAT3_6] << "\n"
+     << mat[MAT3_1] << " " << mat[MAT3_4] << " " << mat[MAT3_7] << "\n"
+     << mat[MAT3_2] << " " << mat[MAT3_5] << " " << mat[MAT3_8] << "\n";
 
   return ss.str();
 }
--- src/test/algebra_test.cpp
+++ src/test/algebra_test.cpp
@@ -2,46 +2,152 @@
 #include <Eigen/Dense>
 
 #include <wmrde/algebra/linalg3.h>
-#include <wmrde/algebra/matrix.h>
+#include <wmrde/util/timer.h>
 
 using namespace wmrde;
 
-// Declare a test
-TEST(TestSuite, addTwoIntsPass)
+
+TEST(TestSuite, addVec3)
 {
-  EXPECT_EQ(addTwoInts(2,2),4);
+  Vec3 a,b,c,c2;
+  Real eps = 1e-6;
+  setVec3(eps, a);
+  setVec3(eps, b);
+
+  Vec3 a_backup; copyVec3(a,a_backup);
+
+  size_t num_iter = 1e7;
+  Timer timer; timer.start();
+  for (size_t iter = 0; iter < num_iter; iter++)
+  {
+    addVec3(a,b,c);
+    copyVec3(c,a);
+//    asm(""); //empty assembly to prevent gcc from optimizing out the loop
+  }
+  timer.stop();
+
+  printf("for %zu iterations of addVec3, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
+
+  //verify with Eigen library
+  {
+    copyVec3(a_backup,a);
+
+    Eigen::Matrix<Real,3,1> a_, b_, c_;
+    copy3(a, a_.data());
+    copy3(b, b_.data());
+
+    timer.start();
+    for (size_t iter = 0; iter < num_iter; iter++)
+    {
+      c_ = a_ + b_;
+      a_ = c_;
+//      asm(""); //empty assembly to prevent gcc from optimizing out the loop
+    }
+    timer.stop();
+    printf("for %zu iterations of Eigen 3x1 vector addition, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
+
+    copy3(c_.data(), c2);
+    printf("c = \n%s", Vec3ToString(c).c_str());
+    printf("c2 = \n%s", Vec3ToString(c2).c_str());
+
+    EXPECT_TRUE(Vec3Equal(c,c2));
+  }
 }
 
-// Declare another test
-TEST(TestSuite, addTwoIntsFail)
+TEST(TestSuite, addMat3)
 {
-  EXPECT_EQ(addTwoInts(2,2),5);
+  Mat3 A,B,C,C2;
+  Real eps = 1e-6;
+  setMat3Diagonal(eps, eps, eps, A);
+  setMat3Diagonal(eps, eps, eps, B);
+
+  Mat3 A_backup; copyMat3(A,A_backup);
+
+  size_t num_iter = 1e7;
+  Timer timer; timer.start();
+  for (size_t iter = 0; iter < num_iter; iter++)
+  {
+    addMat3(A,B,C);
+    copyMat3(C,A);
+//    asm(""); //empty assembly to prevent gcc from optimizing out the loop
+  }
+  timer.stop();
+
+  printf("for %zu iterations of addMat3, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
+
+  //verify with Eigen library
+  {
+    copyMat3(A_backup,A);
+
+    Eigen::Matrix<Real,3,3> A_, B_, C_;
+    copyMat3ToArray(A, A_.data());
+    copyMat3ToArray(B, B_.data());
+
+    timer.start();
+    for (size_t iter = 0; iter < num_iter; iter++)
+    {
+      C_ = A_ + B_;
+      A_ = C_;
+//      asm(""); //empty assembly to prevent gcc from optimizing out the loop
+    }
+    timer.stop();
+    printf("for %zu iterations of Eigen 3x3 matrix addition, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
+
+    copyArrayToMat3(C_.data(), C2);
+    printf("C = \n%s", Mat3ToString(C).c_str());
+    printf("C2 = \n%s", Mat3ToString(C2).c_str());
+
+    EXPECT_TRUE(Mat3Equal(C,C2));
+  }
 }
 
 TEST(TestSuite, multMatMat3)
 {
   Mat3 A,B,C,C2;
+  Real eps = 1e-6;
+  setMat3Diagonal(1+eps, 1+eps, 1+eps, A);
+  setMat3Diagonal(1+eps, 1+eps, 1+eps, B); B[1] = eps;
 
-  setMat3(1.25,2,3,4,5,6,7,8,9,A);
-  setMat3(9,8,7,6,5,4,3,2,1,B);
+  Mat3 A_backup; copyMat3(A,A_backup);
 
-  multMatMat3(A,B,C);
-  printf("C = \n%s", Mat3ToString(C).c_str());
+  size_t num_iter = 1e7;
+  Timer timer; timer.start();
+  for (size_t iter = 0; iter < num_iter; iter++)
+  {
+    multMatMat3(A,B,C);
+    copyMat3(C,A);
+//    asm(""); //empty assembly to prevent gcc from optimizing out the loop
+  }
+  timer.stop();
 
-  Eigen::Matrix<Real,3,3> A_;
-  Eigen::Matrix<Real,3,3> B_;
-  Eigen::Matrix<Real,3,3> C_;
+  printf("for %zu iterations of multMatMat3, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
 
-  //.data() returns a pointer to the data array of the matrix
-  copyMat3ToArray(A, A_.data());
-  copyMat3ToArray(B, B_.data());
-  C_ = A_*B_;
-  copyArrayToMat3(C_.data(), C2);
+  //verify with Eigen library
+  {
+    copyMat3(A_backup,A);
 
-  printf("C2 = \n%s", Mat3ToString(C2).c_str());
+    Eigen::Matrix<Real,3,3> A_, B_, C_;
+    copyMat3ToArray(A, A_.data());
+    copyMat3ToArray(B, B_.data());
 
-  EXPECT_TRUE(Mat3Equal(C,C2));
+    timer.start();
+    for (size_t iter = 0; iter < num_iter; iter++)
+    {
+      C_ = A_*B_;
+      A_ = C_;
+//      asm(""); //empty assembly to prevent gcc from optimizing out the loop
+    }
+    timer.stop();
+    printf("for %zu iterations of Eigen 3x3 matrix multiplication, elapsed time = %f ms\n", num_iter, timer.elapsedTimeMs());
+
+    copyArrayToMat3(C_.data(), C2);
+    printf("C = \n%s", Mat3ToString(C).c_str());
+    printf("C2 = \n%s", Mat3ToString(C2).c_str());
+
+    EXPECT_TRUE(Mat3Equal(C,C2));
+  }
 }
+
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
